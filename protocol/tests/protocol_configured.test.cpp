7973181c977ac52b1d01d46bcb356ee9d033b634
Tim Herreijgers <timtim3001@gmail.com>
2022-05-04 22:38:02 +0200

    Extracted test fixture

--- protocol/tests/protocol_configured.test.cpp
+++ protocol/tests/protocol_configured.test.cpp
@@ -8,81 +8,68 @@
 
 #include <gtest/gtest.h>
 
-TEST(ConfiguredProtocoltest, DataConversionWithincorrectSizeReturnNoData)
+class ConfiguredProtocolTest : public ::testing::Test
 {
-    Protocol protocol;
-    protocol.addDataType(DataType::UINT8);
+protected:
+    void SetUp() override
+    {
+        data.dataUint8 = 10;
+        data.dataUint16 = 4592;
+    }
 
     Data data;
-    data.dataUint8 = 10;
+    Protocol protocol;
+};
+
+TEST_F(ConfiguredProtocolTest, DataConversionWithincorrectSizeReturnNoData)
+{
+    protocol.addDataType(DataType::UINT8);
+
     ASSERT_EQ(protocol.convertToDataEntries(reinterpret_cast<uint8_t *>(&data), 0).size(), 0);
 }
 
-TEST(ConfiguredProtocoltest, DataConversionWithSingleInputAsInputReturnsData)
+TEST_F(ConfiguredProtocolTest, DataConversionWithSingleInputAsInputReturnsData)
 {
-    Protocol protocol;
     protocol.addDataType(DataType::UINT8);
 
-    Data data;
-    data.dataUint8 = 10;
     ASSERT_EQ(protocol.convertToDataEntries(reinterpret_cast<uint8_t *>(&data), 1).size(), 1);
 }
 
-TEST(ConfiguredProtocoltest, DataConversionWithSingleInputAsInputReturnsCorrectDataType)
+TEST_F(ConfiguredProtocolTest, DataConversionWithSingleInputAsInputReturnsCorrectDataType)
 {
-    Protocol protocol;
     protocol.addDataType(DataType::UINT8);
 
-    Data data;
-    data.dataUint8 = 10;
     ASSERT_EQ(protocol.convertToDataEntries(reinterpret_cast<uint8_t *>(&data), 1)[0].type(), DataType::UINT8);
 }
 
-TEST(ConfiguredProtocoltest, DataConversionWithSingleInputAsInputReturnsCorrectData)
+TEST_F(ConfiguredProtocolTest, DataConversionWithSingleInputAsInputReturnsCorrectData)
 {
-    Protocol protocol;
     protocol.addDataType(DataType::UINT8);
-
-    Data data;
-    data.dataUint8 = 10;
 
     auto dataEntries = protocol.convertToDataEntries(reinterpret_cast<uint8_t *>(&data), 1);
     ASSERT_EQ(dataEntries[0].as<uint8_t>(), data.dataUint8);
 }
 
-TEST(ConfiguredProtocoltest, DataConversionWithTwoDifferentInputAsInputReturnsData)
+TEST_F(ConfiguredProtocolTest, DataConversionWithTwoDifferentInputAsInputReturnsData)
 {
-    Protocol protocol;
     protocol.addDataType(DataType::UINT8);
     protocol.addDataType(DataType::UINT16);
 
-    Data data;
-    data.dataUint8 = 10;
-    data.dataUint16 = 4592;
     ASSERT_EQ(protocol.convertToDataEntries(reinterpret_cast<uint8_t *>(&data), 3).size(), 2);
 }
 
-TEST(ConfiguredProtocoltest, DataConversionWithTwoDifferentAsInputReturnsCorrectDataType)
+TEST_F(ConfiguredProtocolTest, DataConversionWithTwoDifferentAsInputReturnsCorrectDataType)
 {
-    Protocol protocol;
     protocol.addDataType(DataType::UINT8);
     protocol.addDataType(DataType::UINT16);
 
-    Data data;
-    data.dataUint8 = 10;
-    data.dataUint16 = 4592;
     ASSERT_EQ(protocol.convertToDataEntries(reinterpret_cast<uint8_t *>(&data), 3)[1].type(), DataType::UINT16);
 }
 
-TEST(ConfiguredProtocoltest, DataConversionWithTwoDifferentAsInputReturnsCorrectData)
+TEST_F(ConfiguredProtocolTest, DataConversionWithTwoDifferentAsInputReturnsCorrectData)
 {
-    Protocol protocol;
     protocol.addDataType(DataType::UINT8);
     protocol.addDataType(DataType::UINT16);
-
-    Data data = {};
-    data.dataUint8 = 10;
-    data.dataUint16 = 4592;
 
     auto dataEntries = protocol.convertToDataEntries(reinterpret_cast<uint8_t *>(&data), 3);
     ASSERT_EQ(dataEntries[1].as<uint16_t>(), data.dataUint16);
