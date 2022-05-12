76e850a69f29ccbff516f539f1124fd83eacbda9
Tim Herreijgers <timtim3001@gmail.com>
2022-05-12 17:53:34 +0200

    Added table model

--- interface/src/mainwindow.cpp
+++ interface/src/mainwindow.cpp
@@ -5,8 +5,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include "protocol_table_item_model.h"
 
 MainWindow::MainWindow() : m_ui(new Ui::MainWindow)
 {
     m_ui->setupUi(this);
+
+    const auto protocolTableItemModel = new ProtocolTableItemModel(m_ui->tableView);
+    m_ui->tableView->setModel(protocolTableItemModel);
 }
